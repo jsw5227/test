d193e186652da1e892ae18c81c6dfec67dc9b9fe
jsw5227 <wakefield515@live.missouristate.edu>
2017-09-29 00:33:31 -0500

    Add files via upload

---
+++ testMessage/src/Client.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+ 
+public class Client
+{
+ 
+    private static Socket socket;
+ 
+    public static void main(String args[])
+    {
+        try
+        {
+        	//replace host with public ip of lightsail instance 
+            String host = "111.111.111.111";
+            int port = 80;
+            InetAddress address = InetAddress.getByName(host);
+            socket = new Socket(address, port);
+ 
+            //Send the message to the server
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+ 
+            String number = "2";
+ 
+            String sendMessage = number + "\n";
+            bw.write(sendMessage);
+            bw.flush();
+            System.out.println("Message sent to the server : "+sendMessage);
+ 
+            //Get the return message from the server
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String message = br.readLine();
+            System.out.println("Message received from the server : " +message);
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            //Closing the socket
+            try
+            {
+                socket.close();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
